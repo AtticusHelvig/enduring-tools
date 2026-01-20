8fd9028331b3a849f2c3f1b0cd91a77170e3d8c1
Atticus Helvig <atticushelvig@gmail.com>
2026-01-20 16:43:59 -0700

    fix: breaking speed behavior

--- src/main/java/net/attikai/mixin/PlayerEntityMixin.java
+++ src/main/java/net/attikai/mixin/PlayerEntityMixin.java
@@ -1,32 +1,52 @@
 package net.attikai.mixin;
 
+import net.attikai.EnduringTools;
 import net.attikai.tag.ModTags;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.PlayerLikeEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin {
+public abstract class PlayerEntityMixin extends PlayerLikeEntity {
     @Shadow @Final
     PlayerInventory inventory;
     @Unique
     private static final float BROKEN_MODIFIER = 1.0f / 8.0f;
 
-    @ModifyVariable(method = "getBlockBreakingSpeed(Lnet/minecraft/block/BlockState;)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getAttributeValue(Lnet/minecraft/registry/entry/RegistryEntry;)D", shift = At.Shift.AFTER), ordinal = 0)
-    private float modifyBreakingSpeed(float value) {
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "getBlockBreakingSpeed(Lnet/minecraft/block/BlockState;)F", at = @At(value = "RETURN"), cancellable = true)
+    private void modifyBreakingSpeed(BlockState block, CallbackInfoReturnable<Float> cir) {
         ItemStack tool = this.inventory.getSelectedStack();
         if (!tool.isIn(ModTags.SHOULD_ENDURE)) {
-            return value;
+            return;
         }
         if (tool.getMaxDamage() - tool.getDamage() != 1) {
-            return value;
+            return;
         }
-        return value * BROKEN_MODIFIER;
+        // emulate vanilla efficiency behavior
+        float efficiency = tool.getMiningSpeedMultiplier(block);
+        if (efficiency > 1f) {
+            efficiency += (float) this.getAttributeValue(EntityAttributes.MINING_EFFICIENCY);
+        }
+        float unbroken = cir.getReturnValueF();
+        // don't be worse than hand mining
+        float result = Math.max(unbroken / efficiency, unbroken * BROKEN_MODIFIER);
+        cir.setReturnValue(result);
     }
 }
