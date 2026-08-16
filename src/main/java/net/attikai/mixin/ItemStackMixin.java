255475362067c59a0b7a200fa16833934bd91ebc
Atticus Helvig <atticushelvig@gmail.com>
2026-08-15 22:14:00 -0700

    chore: port to 26.1

--- src/main/java/net/attikai/mixin/ItemStackMixin.java
+++ src/main/java/net/attikai/mixin/ItemStackMixin.java
@@ -3,8 +3,7 @@ package net.attikai.mixin;
 import net.attikai.tag.ModTags;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemInstance;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
-public abstract class ItemStackMixin {
+public abstract class ItemStackMixin implements ItemInstance {
 
-    @Shadow public abstract boolean is(TagKey<Item> tag);
     @Shadow public abstract int getMaxDamage();
     @Shadow public abstract int getDamageValue();
+    @Shadow public abstract boolean nextDamageWillBreak();
 
     @Inject(method = "processDurabilityChange(ILnet/minecraft/server/level/ServerLevel;Lnet/minecraft/server/level/ServerPlayer;)I", at = @At("HEAD"), cancellable = true)
     private void modifyDamageCalculation(int baseDamage, ServerLevel world, ServerPlayer player, CallbackInfoReturnable<Integer> cir) {
@@ -26,7 +25,7 @@ public abstract class ItemStackMixin {
             return;
         }
         // if it won't break let it be
-        if (getDamageValue() + baseDamage < getMaxDamage()) {
+        if (!this.nextDamageWillBreak()) {
             return;
         }
         // survive on 1 durability
